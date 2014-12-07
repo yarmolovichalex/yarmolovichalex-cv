00c6180f2a90a606aac08c8705cbafd52622f5c4
yarmolovichalex <yarmolovichalex@gmail.com>
2014-12-07 20:12:13 +0300

    initial commit

---
+++ yarmolovichalex-cv/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace yarmolovichalex_cv.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ yarmolovichalex-cv/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="yarmolovichalex_cv.MvcApplication" Language="C#" %>
